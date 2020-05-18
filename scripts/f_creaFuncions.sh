cba583745f96e4c6b00e0d2c8dbb9696cb6bde8f
joanpardogine <63462877+joanpardogine@users.noreply.github.com>
2020-05-18 14:30:58 +0200

    Create f_creaFuncions.sh

---
+++ scripts/f_creaFuncions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Nom:  f_creaFuncions.sh
+  function f_acaba {
+      exit
+   }
+           
+   function f_hola {
+      echo "Hola món!"
+   }
+           
+   f_hola
+   f_acaba
+   echo "no surto mai!"
