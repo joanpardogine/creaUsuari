4ce0a4a138e4df01c40bf25c712a8a9bdcdc449a
joanpardogine <63462877+joanpardogine@users.noreply.github.com>
2020-05-18 16:54:57 +0200

    Millora del fitxer f_interficiesUsuari.sh

--- scripts/f_interficiesUsuari.sh
+++ scripts/f_interficiesUsuari.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 # Nom:  f_interficiesUsuari.sh
-OPTIONS="Hola Surt"
+OPTIONS="Hola Usuari Shell Surt"
   select opt in $OPTIONS; do
     if [ "$opt" = "Surt" ]; then
       echo fet!
       exit
+
     elif [ "$opt" = "Hola" ]; then
-      echo "Hola món!"
+      echo "Hola $USER!"
+
+
+    elif [ "$opt" = "Usuari" ]; then
+      echo "Et dius $USER!"
+
+    elif [ "$opt" = "Shell" ]; then
+      echo "Fas servir $SHELL"
     else
       echo "Opció incorrete!"
     fi
