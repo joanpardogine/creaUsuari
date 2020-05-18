5bbcec31e3ab480c216dd87d7899d909b48e618e
joanpardogine <63462877+joanpardogine@users.noreply.github.com>
2020-05-18 14:31:23 +0200

    Create f_creaFunAmbParam.sh

---
+++ scripts/f_creaFunAmbParam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Nom:  f_creaFunAmbParam.sh
+  function f_acaba {
+    exit
+  }
+           
+  function f_mostra {
+    echo $1 
+  }
+           
+  f_mostra "Hola"
+  f_mostra " món!"
+  f_acaba
+   echo "no surto mai!"
